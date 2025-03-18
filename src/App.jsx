48089713a79ea8a4ef10b2ed2afd938c73409f77
Michelle <elle.scarps@gmail.com>
2025-03-18 13:24:32 -0400

    semantic structure with reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from "./components/ContactList";
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [contacts, setContacts] = useState();
+  console.log("Contacts: ", contacts);
+  
   return (
     <>
-      
+      <ContactList  />
     </>
   )
 }
